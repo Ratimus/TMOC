5ddbd7d8d80715101285de9d8b63ae9d9bea7b84
Ratimus <rynage@gmail.com>
2024-04-05 21:04:41 -0700

    Just add a banner to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+// ------------------------------------------------------------------------
+//                       Turing Machine On Crack
+//
+//   A software re-imagining of Tom Whitwell / Music Thing Modular's Turing
+//   Machine random looping sequencer, plus Voltages and Pulses expanders.
+//
+//   Based on an ESP32 microcontroller and featuring four 12-bit DACs, one
+//   8-bit DAC with analog scale and offset and an inverting output, and
+//   eight gate/trigger outputs
+//
+// main.cpp
+//
+// Nov. 2022
+// Ryan "Ratimus" Richardson
+// ------------------------------------------------------------------------
 #include <Arduino.h>
 #include <Wire.h>
 #include "TMOC_HW.h"
